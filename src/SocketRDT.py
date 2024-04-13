dbe953112a618fcf4b3fe65ef991b97cf184b593
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2024-04-13 15:10:59 -0300

    Cierro los detalles y variables que le faltaban a la clase SocketRDP
    Todo cierra.
    
    Let them cook 🍽️
    
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- src/SocketRDT.py
+++ src/SocketRDT.py
@@ -1,43 +1,52 @@
-from socket import *
-# Con el que estoy hablando
+import socket
 
 class SocketRDT:
-    def __init__(self, tipo, addrPeer, myIP):
+    def __init__(self, tipo, peerAddr, myIP):
         # Este socket representa el socket que yo voy a usar de OUTPUT.
         # Si a mi me quieren hablar. Yo tengo que bindear este socket
         # Si yo quiero hablar con alguien, uso la direccion para hablar
         self.skt = socket.socket(socket.AF_INET, # Internet
                              socket.SOCK_DGRAM) # UDP
         
-        self.skt.bind((UDP_IP, 0))
+        # Cuando bind tiene un 0 de segundo argumento nos da un puerto
+        # nuevo cada vez. Fuente: https://stackoverflow.com/questions/1365265/on-localhost-how-do-i-pick-a-free-port-number
+        # ATTENTION: Necesitamos bindearlo siempre porque tanto cliente
+        # como servidor van a enviar y recibir cosas.
+        self.skt.bind((myIP, 0))
 
+        # ATTENTION: No se si hace falta guardar "mi propio address"
+        # Lo pongo por si lo llegamos a necesitar
+        myPort = self.skt.getsockname()[1]
+        self.myAddress = (myIP, myPort)
 
-        self.IPPeer = addrPeer[0]
-        self.PuertoPeer = addrPeer[1]
-
-        self.IPmy = myIP
-        self.socketMIne = sock.getsockname()[1]
+        self.peerAddr = peerAddr
 
         self.tipo = tipo
 
     def sendall(self, tipo):
         if tipo == "SW":
-            sendall_stop_and_wait()
+            self._sendall_stop_and_wait()
         else:
-            sendall_selective()
+            self._sendall_selective()
         return
 
     def receive_all(self, tipo):
         if tipo == "SW":
-            receive_stop_and_wait()
+            self._receive_stop_and_wait()
         else:
-            receive_selective()
+            self._receive_selective()
         return
 
     def shutdown(self, tipo):
         return
 
-# Socket Servidor    <- Socket Cliente 1
-#       ^
-#       |
-#   Socket Cliente 2
+    def _sendall_stop_and_wait(self,):
+        pass
+    def _sendall_selective(self,):
+        pass
+
+    def _receive_stop_and_wait(self,):
+        pass
+
+    def _receive_selective(self,):
+        pass
